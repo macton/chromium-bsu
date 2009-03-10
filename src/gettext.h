8af42b40e47a4de6794b7faeefac460b7a54d26d
Paul Wise <pabs3@bonedaddy.net>
2009-03-10 09:09:29 +0000

    Fix bug with the i18n patch that prevents building.

--- src/gettext.h
+++ src/gettext.h
@@ -4,12 +4,11 @@
 #if ENABLE_NLS
 	#include <libintl.h>
 	#define _(str) gettext(str)
-	#define N_(str) gettext_noop(str)
+	#define N_(str) str
 #else
 	#define _(str) str
 	#define N_(str) str
 	#define gettext(str) str
-	#define gettext_noop(str) str
 #endif
 
 #endif /* gettext_h */
