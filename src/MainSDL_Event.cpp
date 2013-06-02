aa1f34ad414c6dbba1aa216bcef1a7ba38a42888
Paul Wise <pabs3@bonedaddy.net>
2013-06-02 20:42:51 +0800

    SDL2: Some SDLK_KP* constants were renamed to SDLK_KP_*

--- src/MainSDL_Event.cpp
+++ src/MainSDL_Event.cpp
@@ -28,6 +28,18 @@
 #include "Audio.h"
 #include "Ground.h"
 
+#if SDL_VERSION_ATLEAST(2,0,0)
+		#define SDLK_KP1 SDLK_KP_1
+		#define SDLK_KP2 SDLK_KP_2
+		#define SDLK_KP3 SDLK_KP_3
+		#define SDLK_KP4 SDLK_KP_4
+		#define SDLK_KP5 SDLK_KP_5
+		#define SDLK_KP6 SDLK_KP_6
+		#define SDLK_KP7 SDLK_KP_7
+		#define SDLK_KP8 SDLK_KP_8
+		#define SDLK_KP9 SDLK_KP_9
+#endif
+
 //----------------------------------------------------------
 bool MainSDL::process(SDL_Event *event)
 {
