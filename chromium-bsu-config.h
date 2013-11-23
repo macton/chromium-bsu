db3b829f6461c7fcbb3f794e7ab07acb00f03890
Andreas Fredriksson <dep@defmacro.se>
2013-11-23 11:07:21 -0800

    Adding this file.

---
+++ chromium-bsu-config.h
@@ -0,0 +1,10 @@
+// This is a hard-coded config for our windows build of this game.
+
+#pragma once
+
+#define TEXT_FTGL 1
+#define USE_SDL 1
+#define IMAGE_GLPNG 1
+#define PACKAGE "chromium"
+#define PACKAGE_STRING "chromium whatever"
+
